f858a69a03a2a468f5b4b2f6e06370d6fe223a59
AJTrowell <ajtrowell@gmail.com>
2017-02-27 18:07:04 -0500

    Trump Rotates while falling/jumping
    First bit of rotational animation.

--- Trump.js
+++ Trump.js
@@ -4,14 +4,18 @@ function Trump() {
 this.pos_x = 150; // center position
 this.pos_y = 100; // center position
 this.velocity = 0;
+this.rotation = 0; // Rotation reversed to be counter clockwise, positive for up.
 
 // Game Settings
 this.acceleration = .3; // pixesl per second^2
 this.jump_velocity = -10;
 this.size = 50;
 this.max_fall_velocity = 50;
+this.rotationRate = -30; // Degrees per second, while falling
+this.rotationMax = 30; // Degrees up when jumping.
+this.rotationMin = -70; // Degrees down when falling.
 
-this.DEBUG = true;
+this.DEBUG = true; // Show debug outlines.
 
 // Trump Image Assets
 this.img_head;
@@ -20,6 +24,7 @@ this.img_head;
 
 Trump.prototype.jump = function() {
     this.velocity = this.jump_velocity;
+    this.rotation = this.rotationMax;
 }
 
 Trump.prototype.applyGravity = function() {
@@ -32,6 +37,12 @@ Trump.prototype.applyGravity = function() {
         this.velocity = 0;
         this.pos_y = height - ground_height - this.size/2;
     }
+
+    this.rotation += this.rotationRate / frameRate();
+    if(this.rotation < this.rotationMin) { // Rotated past minimum
+        this.rotation = this.rotationMin;
+    }
+    
 }
 
 Trump.prototype.draw = function() {
@@ -40,7 +51,8 @@ Trump.prototype.draw = function() {
         imageMode(CENTER);
         angleMode(DEGREES);
         translate(this.pos_x,this.pos_y);
-        rotate(20); // Degrees
+        // rotate(20); // Degrees
+        rotate(-this.rotation);
         this.img_head.resize(0,200);
         image(this.img_head,0,0);
     pop()
